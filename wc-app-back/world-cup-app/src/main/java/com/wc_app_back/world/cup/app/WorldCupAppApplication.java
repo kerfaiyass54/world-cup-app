f278186f5096561ac04fcc85afa131dbe303da04
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-04-01 15:36:32 +0100

    Added front and back

---
+++ wc-app-back/world-cup-app/src/main/java/com/wc_app_back/world/cup/app/WorldCupAppApplication.java
@@ -0,0 +1,13 @@
+package com.wc_app_back.world.cup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldCupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldCupAppApplication.class, args);
+	}
+
+}
---
+++ wc-app-back/world-cup-app/src/test/java/com/wc_app_back/world/cup/app/WorldCupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wc_app_back.world.cup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
